d2f5900349e1290b0b73ace4bf664f520597b781
bishwamitre <bishwamitre@outlook.com>
2024-10-02 15:19:07 +0530

    Remove class and object due to low performance and undesired loop

--- autosearch.py
+++ autosearch.py
@@ -2,51 +2,50 @@ import wikipedia
 import random
 import webbrowser
 import time
-import num2words
 
-class RewardSearch:
-    def __init__(self):
-        self.page = wikipedia.random(pages=1)
-        self.url = f"https://www.bing.com/search?q={self.page}&FORM=AWRE"
+def generate_random_page():
+    return wikipedia.random(pages=1)
 
-    def open_web_page(self):
-        webbrowser.open_new_tab(self.url)
+def create_url(page):
+    return f"https://www.bing.com/search?q={page.replace(' ', '+')}&FORM=AWRE"
 
-    def get_integer(self):
-        while True:
-            try:
-                num = int(input("Enter a positive integer: "))
-                if num <= 0:
-                    print("Please enter a positive integer.")
-                else:
-                    return num
-            except ValueError:
-                print("Please enter a valid number. Try again!")
+def open_web_page(url):
+    webbrowser.open_new_tab(url)
 
-    def engine(self, n):
-        for count in range(1, n+1):
-            word = num2words.num2words(count)
-            word = str(word).capitalize()
-            U = (f"{word} searches has performed.")
-            print("{:^40}".format(U))
-            self.open_web_page()
-            sleep_time_range = (25, 40)  # Configurable sleep time range
-            if count != n:
-                sleep_time = random.randint(*sleep_time_range)
-                time.sleep(sleep_time)
-            else:
-                break
-
-    def main(self):
+def get_integer():
+    while True:
         try:
-            number = self.get_integer()
-            print("{:-^40}".format("STARTED"))
-            self.engine(number)
-            print("{:-^40}".format("FINISHED"))
-        except KeyboardInterrupt or EOFError:
-            print("\nInterrupted by user!")
+            num = int(input("Enter a positive integer: "))
+            if num <= 0:
+                print("Please enter a positive integer.")
+            else:
+                return num
+        except ValueError:
+            print("Please enter a valid number. Try again!")
+
+def engine(n):
+    for count in range(1, n+1):
+        prompt = (f"{count} searches has performed.")
+        print("{:^40}".format(prompt))
+        page = generate_random_page()
+        url = create_url(page)
+        open_web_page(url)
+        sleep_time_range = (25, 40)  # Configurable sleep time range
+        if count != n:
+            sleep_time = random.randint(*sleep_time_range)
+            time.sleep(sleep_time)
+        else:
+            break
+
+def main():
+    try:
+        number = get_integer()
+        print("{:-^40}".format("STARTED"))
+        engine(number)
+        print("{:-^40}".format("FINISHED"))
+    except KeyboardInterrupt or EOFError:
+        print("\nInterrupted by user!")
 
 
 if __name__ == "__main__":
-    search = RewardSearch()
-    search.main()
+    main()
